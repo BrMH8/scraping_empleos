ac77a8fa86b4ce3e57ac68d33171c0f6389d3d14
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-06-26 19:48:39 -0600

    [ADD] buscador con texto a buscar añadido

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ import puppeteer from "puppeteer";
     const pagina = await navegador.newPage();
 
     await pagina.goto("https://hireline.io/mx");
+
+    await pagina.waitForSelector(".fa-search", {
+        timeout: 60000
+    })
+
+     //Ingresar elemento a buscar dinámicamentes
+    await pagina.locator("#keyword-input").fill("desarrollador junior");
 }
 )()

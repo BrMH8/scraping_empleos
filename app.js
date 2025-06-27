5f10921978b108009dc05860954e42b4d63b5cbf
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-06-26 20:17:46 -0600

    [ADD] enlaces para caa vancante recuperados

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 import puppeteer from "puppeteer";
 
-(async() => {
-    const navegador = await puppeteer.launch({
-        headless: false,
-        slowMo: 1000
+(async () => {
+  const navegador = await puppeteer.launch({
+    headless: false,
+    slowMo: 1000,
+  });
+
+  const pagina = await navegador.newPage();
+
+  await pagina.goto("https://hireline.io/mx");
+
+  await pagina.waitForSelector("#keyword-input", {
+    timeout: 60000,
+  });
+
+  //Ingresar elemento a buscar dinámicamentes
+  await pagina.locator("#keyword-input").fill("vuejs");
+
+  await pagina.locator("#home-search-btn").click();
+
+  await pagina.waitForSelector("#jobs", {
+    timeout: 60000,
+  });
+
+  const vacantes = await pagina.evaluate(() => {
+    const elementos = Array.from(
+      document.querySelectorAll(".hl-vacancy-card")
+    );
+    
+    return elementos.map((el) => {
+      const enlace = el.getAttribute("href") || "Sin enlace";
+      return enlace
     });
+  });
 
-    const pagina = await navegador.newPage();
-
-    await pagina.goto("https://hireline.io/mx");
-
-    await pagina.waitForSelector(".fa-search", {
-        timeout: 60000
-    })
-
-     //Ingresar elemento a buscar dinámicamentes
-    await pagina.locator("#keyword-input").fill("desarrollador junior");
-}
-)()
+  console.log("Vacantes encontradas:");
+  console.log(vacantes);
+})();

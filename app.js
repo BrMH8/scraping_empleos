6f1dd55046500245116b13ca042c943ee4c833d7
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-06-26 22:56:19 -0600

    [ADD] navegacion entre paginas añadido con recuperación de vacantes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import puppeteer from "puppeteer";
   });
 
   //Ingresar elemento a buscar dinámicamentes
-  await pagina.locator("#keyword-input").fill("vuejs");
+  await pagina.locator("#keyword-input").fill("nodejs");
 
   await pagina.locator("#home-search-btn").click();
 
@@ -23,17 +23,51 @@ import puppeteer from "puppeteer";
     timeout: 60000,
   });
 
-  const vacantes = await pagina.evaluate(() => {
-    const elementos = Array.from(
-      document.querySelectorAll(".hl-vacancy-card")
-    );
-    
+  let vacantesArray = await pagina.evaluate(() => {
+    const elementos = Array.from(document.querySelectorAll(".hl-vacancy-card"));
+
     return elementos.map((el) => {
       const enlace = el.getAttribute("href") || "Sin enlace";
-      return enlace
+      return enlace;
     });
   });
 
+  let existNextBtn = true;
+
+  while (existNextBtn) {
+    const nextBtn = await pagina.evaluate(() => {
+      const existBtn = document.querySelector("a[rel='next']");
+      if (!existBtn) return false;
+      return true;
+    });
+
+    if (nextBtn) {
+      await pagina.locator("a[rel='next']").click();
+
+      await pagina.waitForSelector("#jobs", {
+        timeout: 60000,
+      });
+
+      const vacantes = await pagina.evaluate(() => {
+        const elementos = Array.from(
+          document.querySelectorAll(".hl-vacancy-card")
+        );
+
+        return elementos.map((el) => {
+          const enlace = el.getAttribute("href") || "Sin enlace";
+          return enlace;
+        });
+      });
+
+      vacantesArray = [...vacantesArray, ...vacantes];
+    }
+
+    existNextBtn = nextBtn;
+  }
+
   console.log("Vacantes encontradas:");
-  console.log(vacantes);
+  console.log(vacantesArray);
+  console.log(vacantesArray.length)
+
+  await navegador.close();
 })();

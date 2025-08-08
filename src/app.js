40f0628b3d6a5de8801e0a8af830ada5b101ef36
Bryan <2022049@utsh.edu.mx>
2025-08-07 19:19:47 -0600

    fix[puerto]

--- src/app.js
+++ src/app.js
@@ -23,10 +23,11 @@ app.get("/api/vacantes", async (req, res) => {
   }
 )
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
-  }
-)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 
 
